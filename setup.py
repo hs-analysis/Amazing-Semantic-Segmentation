f43a3c723be80fdfc7aa2440996f363a32b9663b
Sebastian Murgul <DerWaldi@users.noreply.github.com>
2020-04-07 17:17:56 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+setup(name="semantic_segmentation",
+      version="0.2.0",
+      description="Image Segmentation toolkit for keras",
+      author="",
+      author_email='',
+      platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+      license="MIT",
+      url="https://github.com/luyanger1799/Amazing-Semantic-Segmentation",
+      packages=find_packages(),
+)
